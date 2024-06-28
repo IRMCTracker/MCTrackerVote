7e6da6299f0287b9ce93da03f9aedfb40b9caac1
TheWylot <thewylot22@gmail.com>
2024-06-28 18:09:41 +0330

    chore: even more fixes and added todo for supporting Redis/MariaDB & MySQL as Database Systems

--- src/main/java/ir/mctracker/core/database/DataSource.java
+++ src/main/java/ir/mctracker/core/database/DataSource.java
@@ -1,44 +1,41 @@
 package ir.mctracker.core.database;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import ir.jeykey.megacore.config.premade.Storage;
-import ir.mctracker.MCTrackerVote;
 import ir.mctracker.core.database.models.Vote;
-import lombok.Getter;
+import ir.mctracker.MCTrackerVote;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class DataSource {
-    @Getter
     private static ConnectionSource connectionSource;
+    private static Dao<Vote, Integer> voteDao;
 
-    public static void SQLite() throws SQLException, IOException {
-        File file = new File(MCTrackerVote.getInstance().getDataFolder(), "data.db");
+    public DataSource(MCTrackerVote plugin) throws SQLException, IOException {
+        File file = new File(plugin.getDataFolder(), "data.db");
         if (!file.exists()) file.createNewFile();
 
-        String databaseUrl = "jdbc:sqlite:" + MCTrackerVote.getInstance().getDataFolder() + "/data.db";
+        String databaseUrl = "jdbc:sqlite:" + plugin.getDataFolder() + "/data.db";
 
         connectionSource = new JdbcConnectionSource(databaseUrl);
 
         setupTables();
+
+        // TODO: Make the new Storage System Indeed to work with MySQL/MariaDB & Redis
     }
 
-    public static void MySQL() throws SQLException {
-        String databaseUrl = "jdbc:mysql://" + Storage.MYSQL_HOST + ":" + Storage.MYSQL_PORT + "/" + Storage.MYSQL_DB + "?useSSL=false&autoReconnect=true";
-
-        connectionSource = new JdbcConnectionSource(databaseUrl, Storage.MYSQL_USERNAME, Storage.MYSQL_PASSWORD);
-
-        setupTables();
-    }
-
-    public static void setupTables() throws SQLException {
-        MCTrackerVote.setVotesDao(DaoManager.createDao(connectionSource, Vote.class));
+    public void setupTables() throws SQLException {
+        voteDao = DaoManager.createDao(connectionSource, Vote.class);
         TableUtils.createTableIfNotExists(connectionSource, Vote.class);
     }
 
+    public static Dao<Vote, Integer> getVoteDao() {
+        return voteDao;
+    }
 }
